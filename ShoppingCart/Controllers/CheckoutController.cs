32931a2a2b5d3e3ee1251913fe2d7b10763e2e20
Abdullah-228 <abdullahshaikh2812001@gmail.com>
2023-11-17 12:19:42 +0500

    Initial commit

---
+++ ShoppingCart/Controllers/CheckoutController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Infrastructure;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModels;
+
+public class CheckoutController : Controller
+{
+    private readonly DataContext _context;
+
+    public CheckoutController(DataContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Confirmation()
+    {
+ 
+        return View();
+    }
+
+    //public IActionResult fromm()
+    //{
+    //    List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+
+    //    CartViewModel cartVM = new()
+    //    {
+    //        CartItems = cart,
+    //        GrandTotal = cart.Sum(x => x.Quantity * x.Price)
+    //    };
+
+    //    return View("Index", cartVM);
+    //}
+
+    //// Handle the form submission
+    //[HttpPost]
+    //public IActionResult Index(CheckoutViewModel model)
+    //{
+    //    if (ModelState.IsValid)
+    //    {
+    //        // Process the checkout and save the order
+    //        // You can add your business logic here
+
+    //        return RedirectToAction("Confirmation");
+    //    }
+
+    //    return View(model);
+    //}
+
+    // Display a confirmation page
+
+}
